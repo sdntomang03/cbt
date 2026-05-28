8b9cb69e898cad39cb415a37f4a857ae5cd9d578
sdntomang03 <sdntomang03jkt@gmail.com>
2026-05-28 20:33:32 +0700

    fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             LevelSeeder::class,
             QuestionSeeder::class,
             AcademicSeeder::class,
+            RolePermissionSeeder::class,
         ]);
     }
 }
