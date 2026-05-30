cd7bea92dccfe597d16b9d050d658e44dffc8847
sdntomang03 <sdntomang03jkt@gmail.com>
2026-05-30 21:56:40 +0700

    fixed

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -8,14 +8,15 @@ class SitemapController extends Controller
 {
     public function index()
     {
-        // Ambil semua ujian yang berstatus publik untuk dimasukkan ke sitemap
         $publicExams = Exam::where('is_public', true)
             ->orderBy('updated_at', 'desc')
             ->get();
 
-        // Kembalikan view sitemap.blade.php dengan header XML
         return response()
             ->view('sitemap', compact('publicExams'))
-            ->header('Content-Type', 'text/xml');
+            ->header('Content-Type', 'text/xml')
+            ->header('Cache-Control', 'no-cache, no-store, must-revalidate') // Mencegah caching
+            ->header('Pragma', 'no-cache')
+            ->header('Expires', '0');
     }
 }
