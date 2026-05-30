6db42d9409f611d9a5d8d418fbbed323d9d61184
sdntomang03 <sdntomang03jkt@gmail.com>
2026-05-31 06:35:47 +0700

    fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,44 +46,45 @@
         }
 
         /* 1. Memaksa Text Alignment untuk Paragraf & Teks Biasa */
-        .prose .ql-align-center {
+        .prose-custom .ql-align-center {
             text-align: center !important;
         }
 
-        .prose .ql-align-right {
+        .prose-custom .ql-align-right {
             text-align: right !important;
         }
 
-        .prose .ql-align-justify {
+        .prose-custom .ql-align-justify {
             text-align: justify !important;
         }
 
         /* 2. Fix List (UL/OL): Agar titik/angka ikut bergeser ke tengah bersama teks */
-        .prose li.ql-align-center,
-        .prose li.ql-align-right {
+        .prose-custom li.ql-align-center,
+        .prose-custom li.ql-align-right {
             list-style-position: inside !important;
         }
 
         /* 3. Fix List (UL/OL): Menghapus padding kiri bawaan Tailwind agar benar-benar simetris di tengah */
-        .prose ul:has(> li.ql-align-center),
-        .prose ol:has(> li.ql-align-center) {
+        .prose-custom ul:has(> li.ql-align-center),
+        .prose-custom ol:has(> li.ql-align-center) {
             padding-left: 0 !important;
         }
 
         /* 4. Fix Gambar (Images): Memaksa gambar yang berada di dalam baris 'center' atau memiliki class 'center' untuk ke tengah */
-        .prose .ql-align-center img,
-        .prose img.ql-align-center {
+        .prose-custom .ql-align-center img,
+        .prose-custom img.ql-align-center {
             display: block !important;
             margin-left: auto !important;
             margin-right: auto !important;
         }
 
         /* Tambahan opsional: Jika ada gambar yang diratakan kanan */
-        .prose .ql-align-right img,
-        .prose img.ql-align-right {
+        .prose-custom .ql-align-right img,
+        .prose-custom img.ql-align-right {
             display: block !important;
             margin-left: auto !important;
             margin-right: 0 !important;
+        }
     </style>
     @stack('styles')
 </head>
