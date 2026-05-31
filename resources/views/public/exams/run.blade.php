3124f8018fce16e11cff7bebe6b96296627c0a59
sdntomang03 <sdntomang03jkt@gmail.com>
2026-05-31 20:38:56 +0700

    fixed

--- resources/views/public/exams/run.blade.php
+++ resources/views/public/exams/run.blade.php
@@ -640,7 +640,8 @@
         <form id="finish-form" action="{{ route('public.exams.finish', $exam) }}" method="POST" style="display:none;">
             @csrf</form>
     </div>
-    <script src="path/to/chartjs/dist/chart.umd.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0"></script>
     <script>
         window.isExitingExam = false;
             window.isSystemPopup = false;
