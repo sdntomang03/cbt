220c455b2e17dd3985e11023c107549f5dc9fc78
sdntomang03 <sdntomang03jkt@gmail.com>
2026-06-05 13:20:09 +0700

    fixed

--- app/Http/Controllers/PublicExamController.php
+++ app/Http/Controllers/PublicExamController.php
@@ -305,7 +305,20 @@ class PublicExamController extends Controller
                             }
                         }
                         $isAllCorrect ? $correctCount++ : $wrongCount++;
-                    } else {
+                    } elseif ($q->type === 'essay') {
+    $correctRaw = $q->options->first()->option_text ?? '';
+    $cleanCorrect = trim(strip_tags(html_entity_decode($correctRaw)));
+    $cleanUser = trim(strip_tags($userAnswer));
+
+    $isCorrect = false;
+
+    if (strcasecmp($cleanCorrect, $cleanUser) === 0) {
+        $isCorrect = true;
+    } elseif (is_numeric($cleanCorrect) && is_numeric($cleanUser)) {
+        if ((float) $cleanCorrect === (float) $cleanUser) {
+            $isCorrect = true;
+        }
+    } else {
                         $wrongCount++;
                     }
                 }
