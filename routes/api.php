93bf4c39baea687a2737bf8c2705a1e977f21fca
sdntomang03 <sdntomang03jkt@gmail.com>
2026-06-08 13:54:53 +0700

    fixed

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::prefix('public/exams')->group(function () {
     Route::get('/', [ApiPublicExamController::class, 'index']);
     Route::get('/{slug}/detail', [ApiPublicExamController::class, 'detail']);
     Route::get('/{exam}/ranking', [ApiPublicExamController::class, 'getRanking']);
+    Route::get('/ranking-nasional', [ApiPublicExamController::class, 'nationalRanking']);
 
     // PERBAIKAN DI SINI: Sesuaikan nama fungsinya!
     Route::get('/{exam}/verify', [ApiPublicExamController::class, 'getVerificationCode']);
